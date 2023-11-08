6fd5102d800e75c54ab653d0b7c9de363e19473a
Hồ Quang Tùng <tunghq99@gmail.com>
2023-11-08 17:56:14 +0700

    first commit

---
+++ BaiTestAppMobile/app/src/main/java/com/example/baitest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.baitest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_main)
+    }
+}
